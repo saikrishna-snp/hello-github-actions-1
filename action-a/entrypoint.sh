e99a6e3d0a29bf20fe82f6f82e787d719a73daf4
marvelamar <39518383+marvelamar@users.noreply.github.com>
2021-10-21 17:56:46 +0530

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
 sh -c "echo Hello world my name is $INPUT_MY_NAME"
-sh -c "echo This is my first project demo at $INPUT_COMPANY_NAME"
+sh -c "echo This is my first project demo at $COMPANY_NAME"
